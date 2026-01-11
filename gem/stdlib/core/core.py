eb56f1bb4c3c83347efe74a078edae8f16351943
LemurGamingYT <alexdaltontubb@gmail.com>
2026-01-11 12:55:25 +0000

    Update core.py

--- gem/stdlib/core/core.py
+++ gem/stdlib/core/core.py
@@ -1,14 +1,6 @@
-from gem.lib import Lib, DefinitionContext, builtin
-from gem import ir
+from gem.lib import Lib
 
 
 class core(Lib):
     def init(self):
-        @builtin(self, self.scope.type_map.get('int'), [
-            ir.Param(ir.Position.zero(), self.scope.type_map.get('int'), 'a'),
-            ir.Param(ir.Position.zero(), self.scope.type_map.get('int'), 'b')
-        ])
-        def test_add(ctx: DefinitionContext):
-            a = ctx.arg_value(0)
-            b = ctx.arg_value(1)
-            return ctx.builder.add(a, b, 'sum')
+        pass
